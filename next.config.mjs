98b33ef838905a76ed477a06a8516b558b02f594
mdnehal01 <mdnehalakhlaque43@gmail.com>
2024-12-25 21:20:41 +0530

    Added the next.config.mjs and also setup for supabase

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "kezskricfbuksihuhhqt.supabase.co",
+            "storage.googleapis.com"
+        ]
+    }
+};
+
+export default nextConfig;
