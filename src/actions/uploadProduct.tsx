c90935a5370c7fb3f49fa1d6af3f8215947bc334
mdnehal01 <mdnehalakhlaque43@gmail.com>
2025-02-22 18:17:10 +0530

    Product starts to upload online GOOGLE CLOUD

--- src/actions/uploadProduct.tsx
+++ src/actions/uploadProduct.tsx
@@ -1,7 +1,5 @@
 "use server"
 import { Storage } from "@google-cloud/storage";
-import toast from "react-hot-toast";
-
 
 const projectId = 'campus-bazaar-449007';
 
@@ -11,15 +9,20 @@ export const UploadProduct = async (form: FormData) => {
         const size = form.get('size');
         const brand = form.get('brand');
         const file = form.get('file') as File;
-        const user = "inshaxkhan";
-        ``
+
+        // DONE: user will be called the username who is logged in or userID who is uploading 
+        const userId = form.get('userId');
+
+        const finalFileName = `uid-${userId}/${category}/${file.name}`
+
+        const imagePath = `https://storage.googleapis.com/campus-bazaar/${finalFileName}`
     
         const buffer = await file.arrayBuffer();
         const storage = new Storage(); 
         
-        await storage.bucket("campus-bazaar").file(file.name).save(Buffer.from(buffer)); 
+        await storage.bucket(process.env.BUCKET_NAME!).file(finalFileName).save(Buffer.from(buffer)); 
     
-        toast.success(`The category is ${category} and size is ${size} also brand is ${brand} and file is ${file}`)
+        console.log(`The category is ${category} and size is ${size} also brand is ${brand} and file is ${file} and path is ${imagePath}`)
     } catch (error) {
         console.log(error)
     }
--- src/components/ProductAddForm.tsx
+++ src/components/ProductAddForm.tsx
@@ -7,6 +7,9 @@ import Categories from './Categories'
 import InputType1 from './customs/inputType1'
 import "./css/ProductAddForm.css"
 import { UploadProduct } from '@/actions/uploadProduct'
+import { useUser } from '@/hooks/useUser'
+import { FaCamera } from 'react-icons/fa'
+import { BiSolidCameraPlus } from 'react-icons/bi'
 
 let selectedCategory = "Choose a category"
 let disp = "hidden";
@@ -30,6 +33,8 @@ const ProductAddForm = () => {
 // chooseSize = <div><InputType1 onChange={handleSizeChange} needIcon={false} placeholder='Enter size' value={size}/><br /></div
     const [brandValue, setBrandValue] = useState("");
     const [size, setSize] = useState("");
+
+    const {user} = useUser();
     // const [category, setCategory] = useState("");
 
     const handleBrandChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -62,31 +67,19 @@ const ProductAddForm = () => {
             </div>
 
             <InputType1 name='size' className={`${disp} w-64 h-10 focus:border-blue-300`} onChange={handleSizeChange} needIcon={false} placeholder='Enter size' value={size}/>
+            <input className='hidden' type="text" name='userId' value={user?.id ?? ""} onChange={()=>{}}/>
 
             {/* TODO: Already specified brands */}
             {/* <BrandBtn/> */}
 
             {/* <BrandInputBox/> */}
+            
             <InputType1 onChange={handleBrandChange} name='brand' value={brandValue} needIcon={false} placeholder='Specify Brand Name' title='Specify brand'  className='w-64 h-10 focus:border-blue-300'/>
 
-            {/* <Image src={"/logo/brands/cart.png"} alt='cart' width={200} height={300}/> */}
-
-
-            {/* <label className="file-upload-label" htmlFor="file">
-                <div className="file-upload-design">
-                    <svg height="1em" viewBox="0 0 640 512">
-                        <path
-                            d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
-                        ></path>
-                    </svg>
-                    <p>Drag and Drop</p>
-                    <p>or</p>
-                    <span className="browse-button">Browse file</span>
-                </div>
-                <input type="file" id="file" name="file" />
-            </label> */}
-
-            <input type="file" name='file'/>
+            <div className='relative border-2 grid place-items-center border-pink-700 h-20 w-20 rounded-sm mt-2 cursor-pointer bg-slate-100 hover:bg-slate-200 transition-all duration-300'>
+                <BiSolidCameraPlus size={30} className='text-gray-400' />
+                <input className='absolute cursor-pointer w-full h-full opacity-0' type="file" name='file'/>
+            </div>
 
 
             <ButtonType1 className='bg-pink-900 rounded-md w-40 text-white absolute bottom-3 right-3' type='submit'>Add</ButtonType1>
