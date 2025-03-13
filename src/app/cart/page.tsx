a1dc464b2593292074eec643077299775271a08a
Insha Khan <khaninsha0001@gmail.com>
2025-03-14 02:35:13 +0530

    design cart without login page & empty cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className='h-[calc(100vh-100px)] w-full bg-pink-800 grid place-items-center'>
-        <div className='bg-white/90 h-[500px] w-[1000px] rounded-3xl shadow-md shadow-black '>
-        {/* <h1 className=''>Your Cart Looks Empty? </h1>
-        <button className='bg-pink-900 border-2 rounded-xl text-white font-sans font-semibold py-2 px-3 hover:scale-105 transition-all duration-200 hover:border-pink-900 hover:bg-white hover:text-pink-900'>Start Shopping</button>
-        <button className='bg-pink-900 border-2 rounded-xl text-white font-sans font-semibold py-2 px-3 hover:scale-105 transition-all duration-200 hover:border-pink-900 hover:bg-white hover:text-pink-900'>Login</button> */}
-        <CartContent/>
+    <div className='h-[calc(100vh-100px)] w-full bg-pink-100 grid place-items-center'>
+        <div className='bg-white/90 h-[500px] w-[1000px] rounded-xl shadow-md shadow-gray-400 '>
+              
+          
+          <CartContent/>
         </div>
     </div>
   )
--- src/components/CartContent.tsx
+++ src/components/CartContent.tsx
@@ -8,37 +8,60 @@ const CartContent = () => {
     const pathname = usePathname(); // Get the current route path
     const {user, userDetails, isLoading}=useUser();
   return (
-    <div className='flex p-28  rounded-t-3xl'>
-          <div className='h-[200px] w-[350px] bg-yellow-200 p-5'>
-            {/* add cart image */}
-            
-          </div>
+    <div className='flex rounded-t-3xl'>
 
-          <div className='p-5  w-full'>
+          <div className='w-full'>
+
             {user ? <>
-          <h1>It looks like your cart is empty</h1>
-          <button className='bg-pink-900 border-2 rounded-xl text-white font-sans font-semibold py-2 px-3 hover:scale-105 transition-all duration-200 hover:border-pink-900 hover:bg-white hover:text-pink-900' onClick={()=>{
-                router.push("/products");
-              }}>Start Shopping</button>
-              </>
+
+              <div className="loggedIn flex items-center justify-center pt-20">
+
+                <div className='h-[300px] w-[300px] bg-yellow-200 bg-center bg-contain' 
+                    style={{backgroundImage:`url(/cart/campus-bazaar-cart-empty.jpeg)`}}
+                    >
+                </div>
+
+                <div className= " h-[300px] w-[500px] pt-20 flex flex-col  gap-y-6">
+                  <h1 className='text-3xl font-bold font-sans'>Oh no! Your cart is empty</h1>
+                  <button className='w-[200px] bg-green-600 border-2 rounded-md text-white font-sans font-semibold py-1 px-3 hover:scale-105 transition-all duration-200 hover:border-pink-600 hover:bg-white hover:text-pink-900' onClick={()=>{
+                    router.push("/products");
+                  }}>Start Shopping</button>
+                  <h1 className='text-xl text-pink-700 font-sans'>Your cart is on a diet. Feed it with some cool finds!</h1>
+                </div>
+              
+              </div>
+              
+            </>
+
+
            : 
             <>
-              <h1>It looks like you&apos;re not logged in</h1>
-              <button 
-                className='
-                bg-pink-900 border-2 
-                rounded-xl text-white font-sans font-semibold 
-                py-2 px-3 hover:scale-105 transition-all 
-                duration-200 hover:border-pink-900 hover:bg-white hover:text-pink-900' 
-                onClick={()=>{
-                router.push(`/login?redirectTo=${pathname}`); // Pass the current path
-                }}
-                >
-             Login 
-             </button>  
+
+              <div className='loggedOut flex items-center justify-center pt-20'>
+                <div className='h-[300px] w-[300px] bg-pink-50 bg-center bg-contain' 
+                    style={{backgroundImage:`url(/cart/boo-bears.png)`}}
+                    >
+                </div>
+
+                <div className='h-[300px] w-[500px] pt-20 flex flex-col  gap-y-6 pl-8'>
+                  <h1 className='text-3xl font-bold font-sans'>Looks like you&apos;re not logged in</h1>
+                  <button className='w-[200px] bg-green-600 border-2 rounded-xl text-white font-sans font-semibold py-1 px-2 
+                    hover:scale-105 transition-all duration-200 hover:border-pink-900 hover:bg-white hover:text-pink-900' 
+                    onClick={()=>{
+                    router.push(`/login?redirectTo=${pathname}`); // Pass the current path
+                    }}>
+                  
+                    Login 
+                  </button>
+
+                  <h1 className='text-xl text-pink-700 font-sans'>No login, no loot. It’s the rule of the bazaar</h1>
+                </div>
+
+              </div>
+  
             </>
-          
           }
+          
           </div>
           
     </div>
