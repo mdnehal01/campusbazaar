a376774f715e16172827f04d1033e6db1ae95bc9
mdnehal01 <mdnehalakhlaque43@gmail.com>
2025-04-19 10:49:57 +0530

    CARBON

--- src/components/BuyItems.tsx
+++ src/components/BuyItems.tsx
@@ -17,6 +17,9 @@ const BuyItems: React.FC<BuyItemsProps> = ({ products }) => {
 
   const formattedPrice = new Intl.NumberFormat('en-IN').format(totalPrice)
 
+  // TODO: Carbon percentage from cart products
+  const [carbon, setCarbon] = useState("20");
+
   return (
     <div className='bg-pink-50 rounded-r-xl p-5 w-full h-full'>
       {products.length == 0 ? (
@@ -24,9 +27,10 @@ const BuyItems: React.FC<BuyItemsProps> = ({ products }) => {
           <GlobeDemo/>
         </>
       ):(
-        <>
-          <span className='text-sm'>Your Total:</span> <span className='text-md font-semibold'>₹{formattedPrice}</span>
-        </>
+        <div className='flex flex-col gap-5'>
+          <span>Woohoo you&apos;re going to save <span className='text-green-600'>{carbon}</span> tons of Carbon waste</span>
+          <span><span className='text-sm'>Your cart total:</span> <span className='text-md font-semibold'>₹{formattedPrice}</span></span>
+        </div>
       )}
     </div>
   )
